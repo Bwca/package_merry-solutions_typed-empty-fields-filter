c7b52c2d0712eee649c517f448416fb8180c23d7
Volodymyr Yepishev <i.m.bwca@gmail.com>
2022-11-06 06:32:48 -0500

    Added tests

---
+++ src/filter-empty-fields/filter-empty-fields.function.spec.ts
@@ -0,0 +1,65 @@
+import { User } from '../test/user.model';
+import { filterEmptyFields } from './filter-empty-fields.function';
+
+const USERS: Array<User> = [
+  {
+    id: 1,
+    surname: 'Tann',
+    middleName: 'XtraCare Wet Wipes',
+    phone: '750-476-2058',
+    hobby: 'transform one-to-one vortals',
+  },
+  {
+    id: 2,
+    name: 'Emylee',
+    surname: 'Langelaan',
+    middleName: undefined,
+    phone: null,
+  },
+  {
+    id: 3,
+    name: 'Silvanus',
+    surname: 'Bewsy',
+    middleName: 'daytime cold and flu',
+    phone: '999-831-0154',
+    hobby: null,
+  },
+  {
+    id: 4,
+    name: 'Leroi',
+    surname: 'Dragge',
+    middleName: 'Monistat 7 Combination Pack',
+    phone: null,
+    hobby: 'evolve interactive vortals',
+  },
+];
+
+describe('Tests for filtering out objects with null or undefined fields', () => {
+  it('Should filter out objects based on value of a property', () => {
+    // Act
+    const filteredUsers = filterEmptyFields(USERS, 'hobby');
+
+    // string
+    filteredUsers[0]?.hobby;
+
+    // Assert
+    expect(filteredUsers.every((u) => u.hobby !== null && u.hobby !== undefined)).toBeTruthy();
+  });
+  it('Should filter out objects based on value of a list of properties', () => {
+    // Act
+    const filteredUsers = filterEmptyFields(USERS, ['name', 'middleName', 'phone', 'hobby']);
+
+    // string
+    filteredUsers[0]?.name;
+    filteredUsers[0]?.middleName;
+    filteredUsers[0]?.phone;
+    filteredUsers[0]?.hobby;
+
+    // Assert
+    expect(
+      filteredUsers.every((user) =>
+        ['name', 'middleName', 'phone', 'hobby'].every((key) => user[key as keyof User] !== null && user[key as keyof User] !== undefined)
+      )
+    ).toBeTruthy();
+  });
+});
---
+++ src/filter-undefined-fields/filter-undefined-fields.function.spec.ts
@@ -0,0 +1,63 @@
+import { User } from '../test/user.model';
+import { filterUndefinedFields } from './filter-undefined-fields.function';
+
+const USERS: Array<User> = [
+  {
+    id: 1,
+    surname: 'Tann',
+    middleName: 'XtraCare Wet Wipes',
+    phone: '750-476-2058',
+    hobby: 'transform one-to-one vortals',
+  },
+  {
+    id: 2,
+    name: 'Emylee',
+    surname: 'Langelaan',
+    middleName: undefined,
+    phone: '495-141-0246',
+  },
+  {
+    id: 3,
+    name: 'Silvanus',
+    surname: 'Bewsy',
+    middleName: 'daytime cold and flu',
+    phone: '999-831-0154',
+    hobby: null,
+  },
+  {
+    id: 4,
+    name: 'Leroi',
+    surname: 'Dragge',
+    middleName: 'Monistat 7 Combination Pack',
+    phone: '475-277-6642',
+    hobby: 'evolve interactive vortals',
+  },
+];
+
+describe('Tests for filtering out objects with undefined fields', () => {
+  it('Should filter out objects based on value of a property', () => {
+    // Act
+    const filteredUsers = filterUndefinedFields(USERS, 'name');
+    
+    // string
+    filteredUsers[0]?.name
+
+    // Assert
+    expect(filteredUsers.every((u) => u.name !== undefined)).toBeTruthy();
+  });
+  it('Should filter out objects based on value of a list of properties', () => {
+    // Act
+    const filteredUsers = filterUndefinedFields(USERS, ['name', 'middleName', 'hobby']);
+
+    // string
+    filteredUsers[0]?.name
+    filteredUsers[0]?.middleName
+    // string | null
+    filteredUsers[0]?.hobby
+
+    // Assert
+    expect(
+      filteredUsers.every((user) => ['name', 'middleName', 'hobby'].every((key) => user[key as keyof User] !== undefined))
+    ).toBeTruthy();
+  });
+});
